bf2a0129d5527a3b3085ac4c40e7ab260ef66e0a
JoyceHxw <114238035+JoyceHxw@users.noreply.github.com>
2023-04-01 22:42:11 +0800

    Add files via upload

---
+++ Leetcode-Stack/94_Easy_Stack.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+#include <stack>
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        // 注意迭代的顺序
+        vector<int> res;
+        stack<TreeNode*> stk;
+        while (root != nullptr || !stk.empty()) {
+            while (root != nullptr) {
+                stk.push(root);
+                root = root->left;
+            }
+            root = stk.top();
+            stk.pop();
+            res.push_back(root->val);
+            root = root->right;
+        }
+        return res;
+
+    }
+};
