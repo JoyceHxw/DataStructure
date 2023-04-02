2cf139862b1afebf15f93d11813e8b690cd159aa
JoyceHxw <114238035+JoyceHxw@users.noreply.github.com>
2023-04-02 11:58:28 +0800

    Add files via upload

---
+++ Leetcode-Stack/225_Easy_Stack.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+#include <queue>
+
+
+// 注意队列的用法
+class MyStack {
+public:
+    queue<int> queue1;
+    queue<int> queue2;
+
+    /** Initialize your data structure here. */
+    MyStack() {
+
+    }
+
+    /** Push element x onto stack. */
+    void push(int x) {
+        queue2.push(x);
+        while (!queue1.empty()) {
+            queue2.push(queue1.front());
+            queue1.pop();
+        }
+        swap(queue1, queue2);
+    }
+    
+    /** Removes the element on top of the stack and returns that element. */
+    int pop() {
+        int r = queue1.front();
+        queue1.pop();
+        return r;
+    }
+    
+    /** Get the top element. */
+    int top() {
+        int r = queue1.front();
+        return r;
+    }
+    
+    /** Returns whether the stack is empty. */
+    bool empty() {
+        return queue1.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
