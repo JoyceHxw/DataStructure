559e4fe2bcd5afcfdc373c63be6b27c0a7ba6c95
JoyceHxw <114238035+JoyceHxw@users.noreply.github.com>
2023-08-22 11:07:43 +0800

    第二遍

--- Leetcode-Recursion/22_Medium_Recursion.cpp
+++ Leetcode-Recursion/22_Medium_Recursion.cpp
@@ -53,28 +53,61 @@ using namespace std;
 // };
 
 // 答案: 回溯(DFS)
+// class Solution {
+//     void backtrack(vector<string>& ans, string& cur, int open, int close, int n) {
+//         if (cur.size() == n * 2) {
+//             ans.push_back(cur);
+//             return;
+//         }
+//         if (open < n) {
+//             cur.push_back('(');
+//             backtrack(ans, cur, open + 1, close, n);
+//             cur.pop_back();
+//         }
+//         if (close < open) {
+//             cur.push_back(')');
+//             backtrack(ans, cur, open, close + 1, n);
+//             cur.pop_back();
+//         }
+//     }
+// public:
+//     vector<string> generateParenthesis(int n) {
+//         vector<string> result;
+//         string current;
+//         backtrack(result, current, 0, 0, n);
+//         return result;
+//     }
+// };
+
+// 第二遍
+// 对于回溯已经较熟练了，顺利写出
 class Solution {
-    void backtrack(vector<string>& ans, string& cur, int open, int close, int n) {
-        if (cur.size() == n * 2) {
-            ans.push_back(cur);
-            return;
-        }
-        if (open < n) {
-            cur.push_back('(');
-            backtrack(ans, cur, open + 1, close, n);
-            cur.pop_back();
-        }
-        if (close < open) {
-            cur.push_back(')');
-            backtrack(ans, cur, open, close + 1, n);
-            cur.pop_back();
-        }
-    }
 public:
     vector<string> generateParenthesis(int n) {
-        vector<string> result;
-        string current;
-        backtrack(result, current, 0, 0, n);
-        return result;
+        vector<string> ans;
+        recursion(n,0,"",ans);
+        return ans;
+    }
+    // 统计左括号的剩余个数和差几个右括号平衡
+    void recursion(int left_num, int balance, string str, vector<string>& ans){
+        if(left_num==0 && balance==0){
+            ans.push_back(str);
+            return;
+        }
+        if(left_num==0 && balance>0){
+            str+=')';
+            recursion(left_num, balance-1, str, ans);
+        }
+        if(left_num>0 && balance==0){
+            str+='(';
+            recursion(left_num-1, balance+1, str, ans);
+        }
+        if(left_num>0 && balance>0){
+            str+='(';
+            recursion(left_num-1,balance+1, str, ans);
+            str.pop_back();
+            str+=')';
+            recursion(left_num,balance-1,str,ans);
+        }
     }
 };
