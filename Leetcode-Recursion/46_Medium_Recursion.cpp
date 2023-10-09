0bc6a8e427a34704d34cbd33c01d950cbfe34bc0
JoyceHxw <114238035+JoyceHxw@users.noreply.github.com>
2023-10-09 16:22:02 +0800

    第二遍

--- Leetcode-Recursion/46_Medium_Recursion.cpp
+++ Leetcode-Recursion/46_Medium_Recursion.cpp
@@ -39,4 +39,23 @@ public:
             used[j]=false;
         }
     }
+
+    //交换的方式，节省空间
+    // void backtrack(vector<vector<int>>& res, vector<int>& output, int first, int len){
+    //     // 所有数都填完了
+    //     if (first == len) {
+    //         res.emplace_back(output);
+    //         return;
+    //     }
+    //     for (int i = first; i < len; ++i) {
+    //         // 动态维护数组
+    //         swap(output[i], output[first]);
+    //         // 继续递归填下一个数
+    //         backtrack(res, output, first + 1, len);
+    //         // 撤销操作
+    //         swap(output[i], output[first]);
+    //     }
+    // }
+
+
 };
